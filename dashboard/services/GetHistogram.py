295d0e99bf1eb67d27bc393c14ce61371a384640
Deepansh Lodhi <deepanshlodhi27@gmail.com>
2021-11-16 17:19:34 +0530

    EnergyHistogram

--- dashboard/services/GetHistogram.py
+++ dashboard/services/GetHistogram.py
@@ -10,6 +10,7 @@ from dashboard.constants import Periods
 from dashboard.services.utils import (
     GetDayStartingTS, GetPastFirstDayOfMonthTS, GetPastMondayTS,
     GroupDailyPowerData, GroupWeeklyPowerData, GroupMonthlyPowerData,
+    GroupDailyEnergyData, GroupWeeklyEnergyData, GroupMonthlyEnergyData
 )
 
 logger = logging.getLogger(__name__)
@@ -53,6 +54,50 @@ def getPowerHistogram(input_obj: FetchHistogramInput) -> List[TimestampAndValueO
         grouped_samples = group_mapping[input_obj.Period](samples)
     except Exception as e:
         logger.error(f'FetchPowerHistogram Error: {e}')       
+        return None
+
+    return grouped_samples
+
+
+def getEnergyHistogram(input_obj: FetchHistogramInput) -> List[TimestampAndValueObjectSchema]:
+    input_obj.ToTimeStamp = input_obj.Timestamp
+
+    round_ts_mapping_func: Dict[str, Callable[[int], int]] = {
+        Periods.DailyGroup: GetDayStartingTS,
+        Periods.WeeklyGroup: GetPastMondayTS,
+        Periods.MonthlyGroup: GetPastFirstDayOfMonthTS,
+    }
+
+    input_obj.FromTimeStamp = round_ts_mapping_func[input_obj.Period](
+        input_obj.ToTimeStamp
+    )
+
+    try:
+        query_set = ElectricalData.objects.filter(
+            client_id=input_obj.ClientId,
+            delete_status=not True,
+            generation_time_stamp__gte=input_obj.FromTimeStamp,
+            generation_time_stamp__lt=input_obj.ToTimeStamp
+        )
+    except Exception as e:
+        logger.error(f'FetchEnergyHistogram Error: {e}')       
+        return None
+
+    samples = parse_obj_as(List[ElectricalDataSchema], list(query_set))
+
+    group_mapping: Dict[
+        str,
+        Callable[[List[ElectricalDataSchema]], List[TimestampAndValueObjectSchema]]
+    ] = {
+        Periods.DailyGroup: GroupDailyEnergyData,
+        Periods.WeeklyGroup: GroupWeeklyEnergyData,
+        Periods.MonthlyGroup: GroupMonthlyEnergyData,
+    }
+
+    try:
+        grouped_samples = group_mapping[input_obj.Period](samples)
+    except Exception as e:
+        logger.error(f'FetchEnergyHistogram Error: {e}')       
         return None
 
     return grouped_samples
